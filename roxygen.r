bc5249a80cc2e954309cdbacd0da6141874bfbbb
TszKin Julian Chan <ctszkin@gmail.com>
2014-04-01 08:03:10 -0400

    Updated description file

--- roxygen.r
+++ roxygen.r
@@ -1,2 +1,2 @@
 library(roxygen2)
-roxygenize('simmen', roxygen.dir='simmen',copy.package=FALSE, unlink.target=FALSE)
+roxygenize('simmen', roxygen.dir='simmen',copy.package=FALSE, unlink.target=TRUE)
