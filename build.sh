569bc103e8cd30e4a945232ffb5eac3edd6adf33
TszKin Julian Chan <ctszkin@gmail.com>
2014-04-03 19:59:51 -0400

    update gitignore check.sh and build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 R CMD build simmen
-R CMD INSTALL simmen
 R CMD INSTALL simmen --build
+sleep 1
+rm -r simmen/src-i386
+sleep 1
+rm -r simmen/src-x64
+sleep 1
--- check.sh
+++ check.sh
@@ -1,2 +1,8 @@
 Rscript roxygen.r
+sleep 1
 R CMD check simmen --as-cran
+sleep 1
+rm -r simmen/src-i386
+sleep 1
+rm -r simmen/src-x64
+sleep 1
